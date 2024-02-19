a7e871c9582415b90da0b4af52a3408cdedb96d6
Ramachandran A G <ramacg@microsoft.com>
2024-02-19 11:23:44 +0530

    * introduced gzip compression
    Signed-off-by: Tanmaya Panda <tanmayapanda@microsoft.com>

--- plugins/out_azure_kusto/azure_kusto.h
+++ plugins/out_azure_kusto/azure_kusto.h
@@ -2,7 +2,7 @@
 
 /*  Fluent Bit
  *  ==========
- *  Copyright (C) 2015-2022 The Fluent Bit Authors
+ *  Copyright (C) 2015-2024 The Fluent Bit Authors
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -74,6 +74,9 @@ struct flb_azure_kusto {
     flb_sds_t ingestion_mapping_reference;
 
     int ingestion_endpoint_connect_timeout;
+
+    /* compress payload */
+    int compression_enabled;
 
     /* records configuration */
     flb_sds_t log_key;
