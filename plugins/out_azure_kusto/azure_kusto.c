b357fbbcd40e352cdcec9c2879e2f907f7b5fe1f
Tanmaya Panda <tanmayapanda@microsoft.com>
2024-04-09 10:38:30 +0530

    added buffering condition

--- plugins/out_azure_kusto/azure_kusto.c
+++ plugins/out_azure_kusto/azure_kusto.c
@@ -303,34 +303,29 @@ static int cb_azure_kusto_init(struct flb_output_instance *ins, struct flb_confi
         return -1;
     }
 
-    mk_list_init(&ctx->buffer_files);
-    ctx->current_file_path = NULL;
-    ctx->current_file = NULL;
-    ctx->current_file_size = 0;
-    ctx->ins = ins;
+    if (ctx->buffering_enabled == FLB_TRUE) {
+        mk_list_init(&ctx->buffer_files);
+        ctx->current_file_path = NULL;
+        ctx->current_file = NULL;
+        ctx->current_file_size = 0;
+        ctx->ins = ins;
 
-    type = FLB_FSTORE_FS;
+        type = FLB_FSTORE_FS;
 
-    fs = flb_fstore_create(ctx->buffer_dir, type);
-    if (!fs) {
-        return -1;
-    }
+        fs = flb_fstore_create(ctx->buffer_dir, type);
+        if (!fs) {
+            return -1;
+        }
 
-    // Create buffer directory if it doesn't exist
-    /*if (mkdir(ctx->buffer_dir, 0755) == -1 && errno != EEXIST) {
-        flb_plg_error(ins, "Failed to create buffer directory: %s", ctx->buffer_dir);
-        flb_free(ctx);
-        return -1;
-    }*/
+        sched = flb_sched_ctx_get();
 
-    sched = flb_sched_ctx_get();
-
-    // Start the timer for ingestion to Azure Kusto
-    int ret = flb_sched_timer_cb_create(sched, FLB_SCHED_TIMER_CB_PERM, 60, ingest_to_kusto, ctx, NULL);
-    if (ret == -1) {
-        flb_plg_error(ins, "Failed to start ingestion timer");
-        flb_free(ctx);
-        return -1;
+        // Start the timer for ingestion to Azure Kusto
+        int ret = flb_sched_timer_cb_create(sched, FLB_SCHED_TIMER_CB_PERM, 6000, ingest_to_kusto, ctx, NULL);
+        if (ret == -1) {
+            flb_plg_error(ins, "Failed to start ingestion timer");
+            flb_free(ctx);
+            return -1;
+        }
     }
 
     flb_output_set_context(ins, ctx);
