30bdd3507684beaaf2fad345019805d33ce77b83
Tanmaya Panda <tanmayapanda@microsoft.com>
2024-04-09 11:50:55 +0530

    current file path

--- plugins/out_azure_kusto/azure_kusto.c
+++ plugins/out_azure_kusto/azure_kusto.c
@@ -564,7 +564,7 @@ static void azure_kusto_flush_to_buffer(const void *data, size_t bytes, const ch
         }
 
         // Write JSON data to the new buffer file
-        ret = flb_sds_cat(ctx->current_file, json_data, flb_sds_len(json_data));
+        ret = flb_sds_cat(ctx->current_file_path, json_data, flb_sds_len(json_data));
         if (ret < 0) {
             flb_plg_error(ctx->ins, "Failed to write JSON data to buffer file: %s", ctx->current_file_path);
             flb_sds_destroy(ctx->current_file);
