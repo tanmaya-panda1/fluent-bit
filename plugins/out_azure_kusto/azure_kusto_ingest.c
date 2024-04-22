70b013706fed82f88988ef76dd7490598165503c
Tanmaya Panda <tanmayapanda@microsoft.com>
2024-04-22 12:06:29 +0530

    added logs

--- plugins/out_azure_kusto/azure_kusto_ingest.c
+++ plugins/out_azure_kusto/azure_kusto_ingest.c
@@ -160,9 +160,9 @@ static flb_sds_t azure_kusto_create_blob(struct flb_azure_kusto *ctx, flb_sds_t 
 
     flb_plg_debug(ctx->ins,"inside blob after upstream ha node get");
     u_node->u->base.net.connect_timeout = ctx->ingestion_endpoint_connect_timeout;
-
+    flb_plg_debug(ctx->ins,"inside blob after upstream ha node get  :: setting ingestion timeout");
     u_conn = flb_upstream_conn_get(u_node->u);
-
+    flb_plg_debug(ctx->ins,"inside blob after upstream ha node get :: after getting connection");
     if (u_conn) {
         if (pthread_mutex_lock(&ctx->blob_mutex)) {
             flb_plg_error(ctx->ins, "error unlocking mutex");
