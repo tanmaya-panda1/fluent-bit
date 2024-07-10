ff59e1b34eabad967dbb47ba25bf1cfebfab09bc
Tanmaya Panda <tanmayapanda@microsoft.com>
2024-07-10 21:07:06 +0530

    fixed gzip bug
    Signed-off-by: Tanmaya Panda <tanmayapanda@microsoft.com>

--- plugins/out_azure_kusto/azure_kusto.c
+++ plugins/out_azure_kusto/azure_kusto.c
@@ -496,7 +496,7 @@ static void cb_azure_kusto_ingest(struct flb_config *config, void *data)
 
         flb_plg_debug(ctx->ins, "cb_azure_kusto_ingest ::: before starting kusto queued ingestion %s", file->fsf->name);
 
-        ret = azure_kusto_queued_ingestion(ctx, tag_sds, flb_sds_len(tag_sds), payload, flb_sds_len(payload), NULL);
+        ret = azure_kusto_queued_ingestion(ctx, tag_sds, flb_sds_len(tag_sds), final_payload, final_payload_size, NULL);
         if (ret != 0) {
             flb_plg_error(ctx->ins, "Failed to ingest data to Azure Blob");
         }
